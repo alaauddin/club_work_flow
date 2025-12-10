f2e7fa5403ac35005d979569696141e6070aab64
Alauddin <a.faya@fintechsys.net>
2025-12-10 23:50:02 +0300

    Remove SSL options from PostgreSQL database settings in settings.py
    - Eliminated the 'OPTIONS' section that included the SSL certificate path to simplify the database connection configuration.
    - This change streamlines the settings while maintaining essential database connection parameters.

--- project/settings.py
+++ project/settings.py
@@ -96,9 +96,7 @@ DATABASES = {
         'PASSWORD': 'UZ7Kb?c~KbHd',
         'HOST': '127.0.0.1',
         'PORT': '5432',
-        'OPTIONS': {
-            'sslmode': '/home/spordjei/ssl/khial_sportainmentclub_com_c0153_daa89_1767139199_bc1e759aeac40b9b656f08480a3faa54.crt',
-        },
+
 
     }
 }
